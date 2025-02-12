eb758b98b8dda6e9593fa83abd8766d882ad4e41
dung.nguyen <dung.nguyen3@bmbsoft.com.vn>
2025-02-12 10:48:36 +0700

    feat(trips): implement CRUD operations for trips and integrate car reference

--- src/modules/trips/trips.service.ts
+++ src/modules/trips/trips.service.ts
@@ -1,18 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Trip, TripDocument } from './trip.schema';
+import { Model, Types } from 'mongoose';
+import { Trip, TripDocument } from './schema/trip.schema';
+import { CreateTripDto } from './dto/create-trip.dto';
+import { Car, CarDocument } from '../cars/schema/car.schema';
 
 @Injectable()
 export class TripsService {
-  constructor(@InjectModel(Trip.name) private tripModel: Model<TripDocument>) {}
+  constructor(
+    @InjectModel(Trip.name) private tripModel: Model<TripDocument>,
+    @InjectModel(Car.name) private carModel: Model<CarDocument>,
+  ) {}
 
   async findAll(): Promise<Trip[]> {
-    return this.tripModel.find().exec();
+    return this.tripModel.find().populate('car').exec();
   }
 
-  async create(trip: Trip): Promise<Trip> {
-    const newTrip = new this.tripModel(trip);
-    return newTrip.save();
+  async create(createTripDto: CreateTripDto): Promise<Trip> {
+    const carId = new Types.ObjectId(createTripDto.car);
+
+    const carExists = await this.carModel.findById(carId);
+    if (!carExists) {
+      throw new NotFoundException('Car not found');
+    }
+
+    const newTrip = new this.tripModel({ ...createTripDto, car: carId });
+    return await newTrip.save();
+  }
+
+  async findById(id: string): Promise<Trip> {
+    const existingTrip = await this.tripModel.findById(id).exec();
+    if (!existingTrip) {
+      throw new NotFoundException('Trip not found');
+    }
+    return existingTrip.populate('car');
+  }
+
+  async delelte(id: string): Promise<Trip> {
+    const exitsTrip = await this.tripModel.findByIdAndDelete(id).exec();
+    if (!exitsTrip) {
+      throw new NotFoundException('Trip not found');
+    }
+    return exitsTrip;
+  }
+
+  async updateTrip(id: string, updateTripDto: CreateTripDto): Promise<Trip> {
+    const existingTrip = await this.tripModel.findById(id);
+    if (!existingTrip) {
+      throw new NotFoundException('Trip not found');
+    }
+
+    if (updateTripDto.car) {
+      const carExists = await this.carModel.findById(updateTripDto.car);
+      if (!carExists) {
+        throw new NotFoundException('Car not found');
+      }
+    }
+    const updateTrip = (
+      await this.tripModel.findByIdAndUpdate(
+        id,
+        { $set: updateTripDto },
+        { new: true, runValidators: true },
+      )
+    ).populated('car');
+
+    return updateTrip;
   }
 }
