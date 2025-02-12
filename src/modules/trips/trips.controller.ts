07cfca527fe823ab58f8f9e916e357009b043c9a
dung.nguyen <dung.nguyen3@bmbsoft.com.vn>
2025-02-12 11:31:20 +0700

    refactor(trips): remove DESCRIPTION_MESSAGE import and replace with inline strings in API responses

--- src/modules/trips/trips.controller.ts
+++ src/modules/trips/trips.controller.ts
@@ -11,7 +11,6 @@ import {
 import { TripsService } from './trips.service';
 import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 import { CreateTripDto } from './dto/create-trip.dto';
-import { DESCRIPTION_MESSAGE } from 'src/modules/constants/description-response';
 
 @ApiTags('Trips')
 @Controller('trips')
@@ -20,7 +19,7 @@ export class TripsController {
 
   @Get()
   @ApiOperation({ summary: 'Get all trips' })
-  @ApiResponse({ status: 200, description: DESCRIPTION_MESSAGE[200] })
+  @ApiResponse({ status: 200, description: 'Success' })
   @ApiQuery({ name: 'destination', required: false })
   @ApiQuery({ name: 'limit', required: false, type: Number })
   @ApiQuery({ name: 'index', required: false, type: Number })
@@ -55,12 +54,12 @@ export class TripsController {
   @ApiOperation({ summary: 'Get trip by ID' })
   @ApiResponse({
     status: 200,
-    description: DESCRIPTION_MESSAGE[200],
+    description: 'Success',
     type: CreateTripDto,
   })
   @ApiResponse({
     status: 404,
-    description: DESCRIPTION_MESSAGE[404],
+    description: 'Success',
   })
   getTripById(@Query('id') id: string) {
     return this.tripsService.findById(id);
@@ -68,18 +67,18 @@ export class TripsController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new trip' })
-  @ApiResponse({ status: 201, description: DESCRIPTION_MESSAGE[201] })
-  @ApiResponse({ status: 400, description: DESCRIPTION_MESSAGE[400] })
+  @ApiResponse({ status: 201, description: 'Not Found' })
+  @ApiResponse({ status: 400, description: 'Confict' })
   async createTrip(@Body() trip: CreateTripDto) {
     return this.tripsService.create(trip);
   }
 
   @Delete('/:id')
   @ApiOperation({ summary: 'Delete a trip' })
-  @ApiResponse({ status: 200, description: DESCRIPTION_MESSAGE[200] })
+  @ApiResponse({ status: 200, description: 'Success' })
   @ApiResponse({
     status: 404,
-    description: DESCRIPTION_MESSAGE[404],
+    description: 'Success',
   })
   deleteTrip(@Param('id') id: string) {
     return this.tripsService.delelte(id);
@@ -87,8 +86,8 @@ export class TripsController {
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update a trip' })
-  @ApiResponse({ status: 200, description: DESCRIPTION_MESSAGE[200] })
-  @ApiResponse({ status: 404, description: DESCRIPTION_MESSAGE[404] })
+  @ApiResponse({ status: 200, description: 'Success' })
+  @ApiResponse({ status: 404, description: 'Success' })
   async updateTrip(
     @Param('id') id: string,
     @Body() updateTripDto: CreateTripDto,
